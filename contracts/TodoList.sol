5b90c2f3ec1383fd9f5b8da54e94036a904ea4e2
Anya Singh <anyasingh070605@gmail.com>
2021-10-16 09:34:59 -0400

    setup

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,7 +1,24 @@
 pragma solidity 0.5.0;
 
 contract TodoList{
-    uint public taskCount = 0;
+    uint public taskCount = 0; //written to storage (inside the blockchain)
+
+    struct Task{
+        uint id; //# of the task
+        string content; //what are the tasks
+        bool completed; //checkbox state of the todo list
+    }
+
+    mapping(uint => Task) public tasks;
+    
+    constructor() public{
+        createTask("Make a shopping list");
+    }
+    
+    function createTask (string memory _content) public{
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 
 }
 
